f063a9c16a216007da3b8230852517cfb0b1ce7b
cwryn7whzr-creator <cwryn7whzr@privaterelay.appleid.com>
2026-09-19 17:39:11 +0000

    Deploying to gh-pages from @ cwryn7whzr-creator/storygraph-to-notion@15011f0aedad0f6ca7b277b94f7dfa3a9b1ab9f8 🚀

--- api/getList.js
+++ api/getList.js
@@ -21,7 +21,12 @@ const createStorygraphUrl = (target, username, page = 1) =>
  * @returns {Promise<string>} A promise resolving to the HTML string.
  */
 const fetchPageHtml = async (url) => {
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    },
+  });
   if (!response.ok) {
     throw new Error(`Failed to fetch URL: ${url}, Status: ${response.status}`);
   }
